8e443af9c10faabc65eb6afcf8df92d6d7616c64
bobychaudhary <bobychaudhary708@gmail.com>
2022-04-15 18:37:53 +0530

    initial setup is done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import {Routes, Router, Route} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+function App() {
+  return (
+    <div className="App">
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router> */}
+      <>Boby Chaudhary</>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>This is our home page
+    </div>
+  )
+}
+
+export default Home
