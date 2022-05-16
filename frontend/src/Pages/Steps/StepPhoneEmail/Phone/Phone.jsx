db0ce8b97d637c37c7d0d300f17e92ae7413eca5
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-16 22:02:22 +0530

    changes in steps

--- frontend/src/Pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/Pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Card from '../../../../components/shared/Card/Card';
+import Button from '../../../../components/shared/Button/Button';
 
 const Phone = () => {
   return (
-    <div>
-        This is phone component
-    </div>
+      <Card title="Enter Your Phone Number" icon="phone">
+      
+        <div>
+          <Button  text="Next"></Button>
+        </div>
+
+      </Card>
   )
 }
 
--- frontend/src/Pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/Pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,39 +1,41 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import Phone from './Phone/Phone';
+import Email from './Email/Email';
 import styles from './StepPhoneEmail.module.css'
-import Phone from "./Phone/Phone";
-import Email from "./Email/Email";
-// import styles from "./StepPhoneEmail.module.css";
+
 const phoneEmailMap = {
   phone: Phone,
   email: Email
 }
 
-
 const StepPhoneEmail = ({onNext}) => {
-  const [type,setType]=useState('phone');
-        const Component=phoneEmailMap[type];
-    
-function onNext(){
-        
+
+  const [type, setType] = useState('phone');
+  const Component = phoneEmailMap[type];
+  
+  function onNext(){
+    // setType(type + 1);
+  }
+
   return (
     <>
-    <div className={styles.cardwrapper}>
-      <div className={styles.buttonwrap}>
-      <button onClick={() =>setType('phone')}>
-      Phone
-    </button>
-    <button onClick={() =>setType('email')}>
-      Email
-    </button>
 
+      <div className='cardWrapper'>
+          <div>
+            <div className={styles.buttonWrap} >
+              <button className={styles.tabButton}  onClick = {() => setType('phone')}>
+                  <img src="/images/phone-white.png" alt="phone" />
+              </button>
+              <button className={styles.tabButton}  onClick = {() => setType('email')}>
+                  <img src="/images/mail-white.png" alt="email" />
+              </button>
+            </div>
+            <Component onNext = {onNext}/>
+          </div>
       </div>
-      <Component onNext={onNext} />
 
-    </div>
     </>
   );
-  
-    
-}};
+}
 
 export default StepPhoneEmail;
